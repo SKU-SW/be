e56bbd9e5ff6441a38cb9205c2ba849fc36d3dc7
nahjjun <nahjjun1125@naver.com>
2026-04-16 08:46:16 +0900

    :tada: Start: 프로젝트 시작

---
+++ src/main/java/com/example/sku_sw/SkuSwApplication.java
@@ -0,0 +1,13 @@
+package com.example.sku_sw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkuSwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkuSwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sku_sw/SkuSwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sku_sw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkuSwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
